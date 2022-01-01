7e304d9267ff71c5f6b7bcdad5550daf12190b87
ryg-git <ygretharekar@outlook.com>
2022-01-01 20:58:57 +0530

    leetcode 2001

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,30 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+class Solution {
+    private fun findComp(cnt: Int): Long {
+        var ans = 1L
+        for (i in cnt downTo (cnt - 1)) {
+            ans *= i
+        }
+        return ans / 2
+    }
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    fun interchangeableRectangles(rectangles: Array<IntArray>): Long {
+        val ratios = rectangles.map { it.first().toDouble() / it.last() }
+        val groups = ratios.groupBy { it }
+
+        return groups.keys.fold(0L) { a, e -> a + findComp(groups[e]!!.size) }
+    }
+}
+
+fun main(args: Array<String>) {
+    val s = Solution()
+    println(
+        s.interchangeableRectangles(
+            arrayOf(
+                intArrayOf(4, 8),
+                intArrayOf(3, 6),
+                intArrayOf(10, 20),
+                intArrayOf(15, 30)
+            )
+        )
+    )
 }
