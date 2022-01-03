9b734052cb115ff4e2974b5c5386c5b5603b1fc7
ryg-git <ygretharekar@outlook.com>
2022-01-03 23:01:42 +0530

    [WIP] CF ECR 119 (F)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,13 @@
-fun next()  = readLine()!!.trim()
+fun next() = readLine()!!.trim()
 
+fun solve() {
+
+}
 
 fun main() {
-
-    val x = mutableListOf<IntArray>()
-    val par = IntArray((5*10e5 + 2).toInt()) {it}
-    val ans = mutableListOf<Int>()
-
     val q = next().toInt()
+
     for (i in 1..q) {
-        val inst = next().split(' ').map { it.toInt() }.toIntArray()
-
-        x.add(inst)
+        solve()
     }
-
-    for (i in x.reversed()) {
-        if (i.first() == 1) {
-            ans.add(par[i.last()])
-        } else {
-            par[i[1]] = par[i.last()]
-        }
-    }
-
-    println(ans.reversed().joinToString(" "))
 }
