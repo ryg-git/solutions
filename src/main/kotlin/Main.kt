80c61b22ef7550bbdab96b1b50d33afd13c22823
ryg-git <ygretharekar@outlook.com>
2022-01-07 23:07:55 +0530

    [WIP] CF ECR 119 (F) (5)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,14 @@
 fun next() = readLine()!!.trim()
 
 fun solve() {
+    val nMax = 1 shl 20
     val n = next().toInt()
     val p = next().split(" ").map { it.toInt() }
 
+    for (i in 1..n) {
+        val (a, b, c, d) = listOf(nMax, nMax, nMax, nMax)
 
+    }
 
 }
 
